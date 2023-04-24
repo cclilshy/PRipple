03bc24867a8d9469cfc1dd742da830ed91f36400
cclilshy <jingnigg@gmail.com>
2023-04-24 11:08:50 +0800

    not ws

--- config/route/service.php
+++ config/route/service.php
@@ -3,4 +3,4 @@ use Cclilshy\PRipple\Route\Route;
 
 Route::service("Timer", '\Cclilshy\PRipple\Built\Timer\Timer');
 Route::service("HttpService", '\Cclilshy\PRipple\Built\Http\Service');
-Route::service("WebSocket", '\app\WebSocket\Text');
+//Route::service("WebSocket", '\app\WebSocket\Text');
